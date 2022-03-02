032864e7daf48f24754abb92d113f7ddb4045365
Aaron Van Curen <Aaron.VanCuren@steeldynamics.com>
2022-03-02 08:35:15 -0500

    Created logic for demo product to work

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -12,7 +12,7 @@ namespace Tangy_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
-
+        public bool ShowProps { get; set; }
         public List<Demo_ProductProp> ProductProp { get; set; }
     }
 }
