70f30fc77d6a12b517eb43e5aedbf19cf0eac6a7
Aaron Van Curen <Aaron.VanCuren@steeldynamics.com>
2022-03-07 10:13:06 -0500

    Toastr

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+	if (type === "success") {
+		toastr.success(message, 'Operation Successful', { timeout: 5000 });
+	}
+	if (type === "error") {
+		toastr.error(message, 'Operation Failed', { timeout: 5000 });
+	}
+}
